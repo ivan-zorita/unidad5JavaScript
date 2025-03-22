a0da580d5ad4b656e399ff53d1f2de108d6253c9
Ivan Zorita, 1996 <ivan.zorita@live.u-tad.com>
2025-03-22 17:39:14 +0100

    diseño responsivo y diseños añadidos

--- UD5_Asignatura/main.js
+++ UD5_Asignatura/main.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const preguntas = [
-        { enunciado: "¿Cuál es el planeta más grande del sistema solar?", respuestas: ["Tierra", "Marte", "Júpiter", "Venus"], correcta: 2 },
-        { enunciado: "¿Cuánto es 5 * 5?", respuestas: ["10", "20", "25", "30"], correcta: 2 },
-        { enunciado: "¿En qué año llegó el hombre a la Luna?", respuestas: ["1965", "1969", "1972", "1980"], correcta: 1 },
-        { enunciado: "¿Quién pintó la Mona Lisa?", respuestas: ["Van Gogh", "Picasso", "Da Vinci", "Miguel Ángel"], correcta: 2 },
-        { enunciado: "¿Cuál es el metal más abundante en la corteza terrestre?", respuestas: ["Hierro", "Aluminio", "Cobre", "Plata"], correcta: 1 }
+        { enunciado: "¿Quien fue el primer presidente de esta, nuestra comunidad?", respuestas: ["Juan Cuesta", "Don Bartolomé Mendez Zuloaga", "Marisa Benito", "Doña Carmen Desengaño Sierra"], correcta: 1 },
+        { enunciado: "¿Cual era el nombre pugilistico de Roberto?", respuestas: ["La Mosca de arabia", "el Leon de Montepinar", "El Puma de Cancun", "Robert Patinson "], correcta: 2 },
+        { enunciado: "¿Siguiendo con Roberto.. cual era su famosa muletilla?", respuestas: ["Hombre, pues ya que estamos..", "Para hacer por hacer es mejor deshacer", "Vamos no me jodas", "Venga coño"], correcta: 2 },
+        { enunciado: "Hablando de muletillas... ¿recuerdas cual era la famosa frase de Emilio?", respuestas: ["Un poquito de por favor", "Que pasa niño", "Que te enchufo con la vaporeta", "No me pises lo fregao"], correcta: 0 },
+        { enunciado: "El amor es libre, y mas en esta comunidad,¿pero donde era donde mas se practicaba?", respuestas: ["En la porteria", "En el videoclub", "En casa de las supernenas", "En el cuarto de contadores"], correcta: 3 },
+        { enunciado: "¿Quien era el chechu?", respuestas: ["Un amigo del barrio de Josemi", "El nieto de Concha", "El hijo adoptivo de Mauri y Fernando", "El primo de Josemi"], correcta: 2 },
+        { enunciado: "Pero vamos a ver ignorante de la vida...¿Sabrias decir de quien era?", respuestas: ["Bea", "Belen", "Mariano", "Carlos "], correcta: 2 },
+        { enunciado: "Que mona va esta chica siempre.¿Que chica iba mona siempre?", respuestas: ["Marisa ", "Ana ", "Alicia", "Lucia"], correcta: 3 },
+        { enunciado: "¿Te sabes el eslogan? Deportes Guerra...", respuestas: ["Gimnasio, Casa y Tierra", "Para correr o sacar a la perra", "Playa, Campo y Sierra", "Equipese, y evite una tragedia"], correcta: 2 },
+        { enunciado: "Cuando el coche de Mariano no arranca para perseguir a Juan y tienen que tomar las uvas en el coche,¿quienes estan dentro?", respuestas: ["Mariano y Roberto", "Mariano, Concha y Belen", "Mariano, Andres Guerra, Isabel y pablo", "Mariano, Paloma, Vicenta y Marisa"], correcta: 3 }
     ];
 
     let preguntaActual = 0;
